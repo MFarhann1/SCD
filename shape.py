f81a92cb5c33384e39e83eb0826b249608c3e505
Domain Join <domain@UOL.PK>
2024-02-22 16:08:44 +0500

    third commit

--- shape.py
+++ shape.py
@@ -8,3 +8,31 @@ class shape:
         def perimeter(self):
             pass
             
+class Shape:
+    def __init__(self, color):
+        self.color = color
+        def get_color(self):
+            return self.color
+
+
+             def area(self):
+                pass
+
+class Rectangle(Shape):
+     def __init__(self, color, width, height):
+          super().__init__(color)
+           self.width = width
+            self.height = height
+
+ def area(self):
+    return self.width * self.height
+
+   class Circle(Shape):
+       def __init__(self, color, radius):
+         super().__init__(color)
+           self.radius = radius
+
+def area(self):
+   import math
+    return math.pi * self.radius ** 2
+                        
